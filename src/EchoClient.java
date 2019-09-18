56cb266d55a1960dd2dc1a93e1655281de5b5981
Corentin Thomasset <corentin.thomasset74@gmail.com>
2019-09-18 08:37:56 +0200

    chore: first commit

---
+++ src/EchoClient.java
@@ -0,0 +1,66 @@
+/**
+ * EchoClient
+ * Example of a TCP client
+ * Date: 10/01/04
+ * Authors:
+ */
+
+import java.io.*;
+import java.net.*;
+
+
+public class EchoClient {
+
+
+    /**
+     * main method
+     * accepts a connection, receives a message from client then sends an echo to the client
+     **/
+    public static void main(String[] args) throws IOException {
+
+        Socket echoSocket = null;
+        PrintStream socOut = null;
+        BufferedReader stdIn = null;
+        BufferedReader socIn = null;
+
+        int port = 4242;
+        String host = "127.0.0.1";
+
+        if (args.length == 2) {
+            port = Integer.parseInt(args[1]);
+            host = args[0];
+        }
+
+        System.out.println("Started client on: " + host + ":" + port);
+
+        try {
+            // creation socket ==> connexion
+            echoSocket = new Socket(host, port);
+            socIn = new BufferedReader(
+                    new InputStreamReader(echoSocket.getInputStream()));
+            socOut = new PrintStream(echoSocket.getOutputStream());
+            stdIn = new BufferedReader(new InputStreamReader(System.in));
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host:" + host);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for "
+                    + "the connection to:" + host);
+            System.exit(1);
+        }
+
+        String line;
+        while (true) {
+            line = stdIn.readLine();
+            if (line.equals(".")) break;
+            socOut.println(line);
+            System.out.println("echo: " + socIn.readLine());
+        }
+        socOut.close();
+        socIn.close();
+        stdIn.close();
+        echoSocket.close();
+    }
+}
+
+
---
+++ src/EchoServer.java
@@ -0,0 +1,56 @@
+/***
+ * EchoServer
+ * Example of a TCP server
+ * Date: 10/01/04
+ * Authors:
+ */
+
+import java.io.*;
+import java.net.*;
+
+public class EchoServer {
+
+    /**
+     * receives a request from client then sends an echo to the client
+     *
+     * @param clientSocket the client socket
+     **/
+    static void doService(Socket clientSocket) {
+        try {
+            BufferedReader socIn = null;
+            socIn = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            PrintStream socOut = new PrintStream(clientSocket.getOutputStream());
+            while (true) {
+                String line = socIn.readLine();
+                socOut.println(line);
+            }
+        } catch (Exception e) {
+            System.err.println("Error in EchoServer:" + e);
+        }
+    }
+
+    public static void main(String args[]) {
+        ServerSocket listenSocket;
+        int port = 4242;
+
+        if (args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        System.out.println("Started server on port: " + port);
+
+        try {
+            listenSocket = new ServerSocket(port); //port
+            while (true) {
+                Socket clientSocket = listenSocket.accept();
+                System.out.println("connexion from:" + clientSocket.getInetAddress());
+                doService(clientSocket);
+            }
+        } catch (Exception e) {
+            System.err.println("Error in EchoServer:" + e);
+        }
+    }
+}
+
+  
