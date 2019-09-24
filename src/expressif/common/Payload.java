8cbd9d90ce3c8e476439f4ec8d28e808ddb5be08
Corentin Thomasset <corentin.thomasset74@gmail.com>
2019-09-24 15:52:47 +0200

    feat: wip

---
+++ src/expressif/common/Payload.java
@@ -0,0 +1,16 @@
+package expressif.common;
+
+public class Payload {
+    public enum Topic {LOGIN, JOIN_ROOM, LEAVE_ROOM, LIST_ROOM};
+
+    private Topic topic;
+    private Object content;
+
+    public Topic getTopic() {
+        return topic;
+    }
+
+    public Object getContent() {
+        return content;
+    }
+}
