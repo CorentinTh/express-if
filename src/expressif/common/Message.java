e0f6c59f3d916c97a0731412bc1184d23f57546e
Tania <Tania@192.168.0.20>
2019-09-29 22:36:34 +0200

    feat : tentative de multicast dans Room en commentaire

--- src/expressif/common/Message.java
+++ src/expressif/common/Message.java
@@ -65,32 +65,9 @@ public class Message implements Serializable {
         }
         return null;
     }
-
-public class Message implements Serializable {
-
-    String user;
-    String content;
-    LocalDateTime date;
-
-    public Message(String user, String content, LocalDateTime date) {
-        this.user = user;
-        this.content = content;
-        this.date = date;
-    }
-
-
-    @Override
-    public String toString() {
-        StringBuilder acc = new StringBuilder("{");
-        acc.append("user: '");
-        acc.append(user);
-        acc.append("', content: '");
-        acc.append(content);
-        acc.append("', date: '");
-        acc.append(date);
-        acc.append("'}");
-
-        return acc.toString();
+    
+    public String getContent() {
+    	return this.content;
     }
 
 //    public static void main(String[] args) {
--- src/expressif/server/Room.java
+++ src/expressif/server/Room.java
@@ -4,6 +4,11 @@ import expressif.common.Message;
 import expressif.common.Payload;
 import expressif.common.RoomInfo;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,26 +16,55 @@ public class Room {
     private String name;
     private List<Client> clients = new ArrayList<>();
     private History history;
+    private InetAddress groupAdress;
+    private int groupPort;
+    private MulticastSocket s;
 
     public Room(String name) {
         this.name = name;
         this.history = new LocalStorageHistory(name);
     }
+    
+//    public Room(String name, String inetAddr, int groupPort) throws UnknownHostException{
+//        this.name = name;
+//        this.history = new LocalStorageHistory(name);
+//        this.groupAdress = InetAddress.getByName(inetAddr);
+//        this.groupPort = groupPort;
+//		  this.s =  new MulticastSocket(groupPort);
+//    }
 
     public String getName() {
         return name;
     }
 
     public void joinRoom(Client client) {
+//    	try {
+//			s.joinGroup(this.groupAdress);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
         clients.add(client);
     }
 
     public void leaveRoom(Client client) {
+//    	try {
+//			s.leaveGroup(groupAdress);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
         clients.remove(client);
     }
 
     public void sendMessage(Message message){
         sendData(new Payload(Payload.Topic.NEW_MESSAGE, message));
+//        DatagramPacket dataMessage = new DatagramPacket(message.getContent().getBytes(), message.getContent().length(), this.groupAdress, this.groupPort);
+//        try {
+//			s.send(dataMessage);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
         history.addMessage(message);
     }
 
